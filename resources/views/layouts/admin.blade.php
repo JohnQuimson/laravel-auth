49e907f19cf7cdd486ccbceca1fd1c059921c0b8
JohnQuimson <john.quimson777@gmail.com>
2024-02-06 22:57:14 +0100

    feat: projects carousel

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -65,7 +65,7 @@
                         </div>
                     </nav>
 
-                    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 border border-danger d-flex justify-content-center align-items-center">
                         @yield('content')
                     </main>
                 </div>
