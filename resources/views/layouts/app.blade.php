e9e199f2b9a35e6cd713243269c0c32ae3702cf8
JohnQuimson <john.quimson777@gmail.com>
2024-02-07 17:54:06 +0100

    feat: fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,9 @@
         <title>JQ's Portfolio</title>
 
         <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
         <!-- Usando Vite -->
         @vite(['resources/js/app.js'])
